d1c78e195b97c5c8c54290ef3b550a202ca30c5d
raghav <raghavsehgal129@gmail.com>
2021-01-27 22:14:56 +0530

    update code

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -15,66 +15,18 @@ export class LandingPageComponent implements OnInit {
   selectedYear: any;
   selectedLaunch: any;
   selectedLand: any;
+  developer_name: string = "Raghav Sehgal"
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
+    this.getProgramData(this.paramsObj)
+  }
+
+  getProgramData(obj) {
+    this.dataService.getRequest(obj).subscribe((data: any[]) => {
       this.spacePrograms = this.getFilteredData(data);
     });
-  }
-
-  getYear(val) {
-    if (this.selectedYear === val) {
-      this.selectedYear = null;
-      // this.paramsObj = {};
-      delete this.paramsObj['launch_year'];
-      this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
-        this.spacePrograms = this.getFilteredData(data);
-      });
-    } else {
-      this.selectedYear = val;
-      this.paramsObj['launch_year'] = val;
-      this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
-        this.spacePrograms = this.getFilteredData(data);
-      });
-    }
-  }
-
-  getLaunch(val) {
-    if (this.selectedLaunch === val) {
-      this.selectedLaunch = null;
-      // this.paramsObj = {};
-      delete this.paramsObj['launch_success'];
-      this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
-        this.spacePrograms = this.getFilteredData(data);
-      });
-    } else {
-      this.selectedLaunch = val;
-      this.paramsObj['launch_success'] = val.toLowerCase();
-      this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
-        this.spacePrograms = this.getFilteredData(data);
-      });
-    }
-  }
-
-  getLand(val) {
-    if (this.selectedLand === val) {
-      this.selectedLand = null;
-      // this.paramsObj = {};
-      delete this.paramsObj['land_success'];
-      this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
-        this.spacePrograms = this.getFilteredData(data);
-      });
-    } else {
-      this.selectedLand = val;
-      this.paramsObj['land_success'] = val.toLowerCase();
-      this.dataService.getRequest(this.paramsObj).subscribe((data: any[]) => {
-        this.spacePrograms = this.getFilteredData(data);
-        console.log("this.spacePrograms",this.spacePrograms);
-        
-      });
-    }
   }
 
   getFilteredData(array) {
@@ -82,11 +34,45 @@ export class LandingPageComponent implements OnInit {
       flight_No: val.flight_number,
       mission_name: val.mission_name,
       launch_success: val.launch_success,
-      launch_land: val.rocket.first_stage.cores[0].land_success,
+      launch_land
+        : val.rocket.first_stage.cores[0].land_success,
       mission_id: val.mission_id,
       launch_year: val.launch_year,
       logo: val.links.mission_patch_small
     }));
+  }
+
+  getYear(val) {
+    if (this.selectedYear === val) {
+      this.selectedYear = null;
+      delete this.paramsObj['launch_year'];
+    } else {
+      this.selectedYear = val;
+      this.paramsObj['launch_year'] = val;
+    }
+    this.getProgramData(this.paramsObj)
+  }
+
+  getLaunch(val) {
+    if (this.selectedLaunch === val) {
+      this.selectedLaunch = null;
+      delete this.paramsObj['launch_success'];
+    } else {
+      this.selectedLaunch = val;
+      this.paramsObj['launch_success'] = val.toLowerCase();
+    }
+    this.getProgramData(this.paramsObj)
+  }
+
+  getLand(val) {
+    if (this.selectedLand === val) {
+      this.selectedLand = null;
+      delete this.paramsObj['land_success'];
+    } else {
+      this.selectedLand = val;
+      this.paramsObj['land_success'] = val.toLowerCase();
+    }
+    this.getProgramData(this.paramsObj)
   }
 
 }
